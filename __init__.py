7a10d8102999b4fc5537951ed4713709dd639c1a
Stéphane du Hamel <stephduh@live.fr>
2025-08-11 14:00:48 +0200

    name mappings

--- __init__.py
+++ __init__.py
@@ -4,3 +4,8 @@ NODE_CLASS_MAPPINGS = {
     "WanMoeKSampler":WanMoeKSampler,
     "WanMoeKSamplerAdvanced":WanMoeKSamplerAdvanced
 }
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "WanMoeKSampler": "Wan MoE KSampler",
+    "WanMoeKSamplerAdvanced": "Wan MoE KSampler (Advanced)"
+}
